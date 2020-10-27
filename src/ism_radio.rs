eb59401096648d46a7fcc9bbd95e4770a8788daf
Marshall Clyburn <mardev2013@gmail.com>
2020-10-27 12:19:10 -0400

    Match new kernel interface

--- src/ism_radio.rs
+++ src/ism_radio.rs
@@ -3,15 +3,21 @@ use crate::result::TockResult;
 
 const DRIVER_NUMBER: usize = 0x30003;
 
+#[allow(unused)]
 mod command {
+    pub const AVAILABLE: usize = 0;
     pub const RESET: usize = 1;
-    pub const SET_MODE: usize = 2;
+    pub const STATUS: usize = 2;
+    pub const READ: usize = 3;
+    pub const WRITE: usize = 4;
+    pub const SET_MODE: usize = 5;
 }
 
+#[allow(unused)]
 mod opmode {
     pub const SLEEP: usize = 0;
-    // pub const STANDBY: usize = 1;
-    // pub const FREQUENCYSYNTHESIZER: usize = 2;
+    pub const STANDBY: usize = 1;
+    pub const FREQUENCYSYNTHESIZER: usize = 2;
     pub const TRANSMIT: usize = 3;
     pub const RECEIVE: usize = 4;
 }
@@ -19,6 +25,10 @@ mod opmode {
 pub struct IsmRadioDriver {  }
 
 impl IsmRadioDriver {
+    fn command(command: usize, arg1: usize, arg2: usize) -> TockResult<usize> {
+        Ok(syscalls::command(DRIVER_NUMBER, command, arg1, arg2)?)
+    }
+
     pub fn reset(&self) -> TockResult<usize> {
         let r = syscalls::command(
             DRIVER_NUMBER,
@@ -42,11 +52,19 @@ impl IsmRadioDriver {
         self.set_mode(opmode::SLEEP)
     }
 
+    pub fn standby(&self) -> TockResult<usize> {
+        self.set_mode(opmode::STANDBY)
+    }
+
     pub fn receive(&self) -> TockResult<usize> {
         self.set_mode(opmode::RECEIVE)
     }
 
     pub fn transmit(&self) -> TockResult<usize> {
         self.set_mode(opmode::TRANSMIT)
+    }
+
+    pub fn write(&self, address: u8, value: u8) -> TockResult<usize> {
+        IsmRadioDriver::command(command::WRITE, address as usize, value as usize)
     }
 }
